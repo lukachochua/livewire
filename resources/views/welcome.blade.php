7fa81de50ed2d0c3d5e56abb334926e8b7a160b8
lukachochua <lukachochua@gmail.com>
2022-10-18 00:10:12 +0400

    feat: add Taggle to the projects

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,31 @@
 <x-navbar />
 
 <body>
-    <livewire:poll-example />
+    <div class="my-8">
+        <h2 class="text-lg font-semibold mt-4">Events Example</h2>
+
+        <div class="mt-4">
+           <div 
+                class="border border-black w-1/2 px-4 py-2"
+                x-data
+                x-init="
+                    new Taggle($el, {
+                        tags: ['these', 'are', 'prefilled', 'tags'],
+                        onTagAdd: function(e, tag) {
+                            {{-- alert('You just added ' + tag + '');  --}}
+                        },
+                        onTagRemove: function(e, tag) {
+                            {{-- alert('You just removed ' + tag + '');  --}}
+                        }                         
+
+                    })
+                "    
+            >
+                
+           </div>
+        </div>
+    </div>
+    {{-- <livewire:poll-example /> --}}
 </body>
    
 
